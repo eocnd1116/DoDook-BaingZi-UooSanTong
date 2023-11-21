43b15f54862d88d9ca0e02b5875338812dc148f8
eocnd1116 <68635662+eocnd1116@users.noreply.github.com>
2023-11-21 20:49:02 +0900

    Update final_display_temp.c

--- final_display_temp.c
+++ final_display_temp.c
@@ -1,19 +1,20 @@
 #include <Keypad.h>
 #include <Servo.h> 
 #include <LiquidCrystal_I2C.h>
+#include <Wire.h>
 
 //디스플레이 변수
 LiquidCrystal_I2C lcd(0x27, 16, 2);
 
 void setup() {
   Serial.begin(9600);
-  lcd.begin(16, 2);
-  lcd.clear();
-  lcd.print("LiquidCrystal");
-  lcd.setCursor(0,1);
-  lcd.print("Hello rnjswns");
+  lcd.init();
+  lcd.backlight();
 }
 
 void loop() {
+  lcd.setCursor(0,1);
+  lcd.print("wa sans");
+  lcd.clear();
   
 }
